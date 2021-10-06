bc99c7a6e741d55c140c8411e5d758d6cbfc2c99
roetry <ihb0523@gmail.com>
2021-10-06 21:45:50 +0900

    211006_vc_scene_update
    - remodeling body
    - updating particle

---
+++ scienscope_2021/Assets/Scenes/vacuum_scene_control.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vacuum_scene_control : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
