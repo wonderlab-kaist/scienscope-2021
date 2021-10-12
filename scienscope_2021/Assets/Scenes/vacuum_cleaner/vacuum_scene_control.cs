10755f2d0b2180c886149a5e770e1ad829ca1708
roetry <ihb0523@gmail.com>
2021-10-13 00:23:26 +0900

    21-10-13 turvin_gear slider update
    -turvin speed control add
    -filter layer change

--- scienscope_2021/Assets/Scenes/vacuum_cleaner/vacuum_scene_control.cs
+++ scienscope_2021/Assets/Scenes/vacuum_cleaner/vacuum_scene_control.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class vacuum_scene_control : MonoBehaviour
 {
     public ParticleSystem clip, dust, fine_dust, dirt;
     public GameObject hepa_filter;
     public GameObject turbin;
+    public Slider speed;
 
-    int turvin_gear = 1;
+    private float turvin_gear = 0.5f;
     private float temperature;
     private float filtered;
 
+    private float vacuum_speed_constant = 1.8f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,25 +27,36 @@ public class vacuum_scene_control : MonoBehaviour
     
     void Update()
     {
-        
+        turvin_gear = speed.value;
+
+        turbin.GetComponent<Animator>().speed = turvin_gear;
+
+        clip.startSpeed = vacuum_speed_constant * turvin_gear;
+        dust.startSpeed = vacuum_speed_constant * turvin_gear;
+        fine_dust.startSpeed = vacuum_speed_constant * turvin_gear;
+        dirt.startSpeed = vacuum_speed_constant * turvin_gear;
+
     }
 
     public void particle_burst(int type)
     {
-        switch (type)
+        if(turvin_gear > 1f)
         {
-            case 0:
-                StartCoroutine("burst", clip);
-                break;
-            case 1:
-                StartCoroutine("burst", dirt);
-                break;
-            case 2:
-                StartCoroutine("burst", dust);
-                break;
-            case 3:
-                StartCoroutine("burst", fine_dust);
-                break;
+            switch (type)
+            {
+                case 0:
+                    StartCoroutine("burst", clip);
+                    break;
+                case 1:
+                    StartCoroutine("burst", dirt);
+                    break;
+                case 2:
+                    StartCoroutine("burst", dust);
+                    break;
+                case 3:
+                    StartCoroutine("burst", fine_dust);
+                    break;
+            }
         }
         
     }
@@ -61,4 +75,6 @@ public class vacuum_scene_control : MonoBehaviour
 
         if (pc.emissionRate < 0) pc.emissionRate = 0;
     }
+
+   
 }
