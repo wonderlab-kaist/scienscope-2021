99563d6c851460bfdbd311f82619612ca8c73a0f
roetry <ihb0523@gmail.com>
2021-10-13 20:27:11 +0900

    21-10-13 filter_colliding, color change, sound update
    21-10-13 filter_colliding, color change, sound update

--- scienscope_2021/Assets/Scenes/vacuum_cleaner/vacuum_scene_control.cs
+++ scienscope_2021/Assets/Scenes/vacuum_cleaner/vacuum_scene_control.cs
@@ -6,15 +6,21 @@ using UnityEngine.UI;
 public class vacuum_scene_control : MonoBehaviour
 {
     public ParticleSystem clip, dust, fine_dust, dirt;
+    public ParticleSystem filter_dust;
     public GameObject hepa_filter;
     public GameObject turbin;
     public Slider speed;
+
 
     private float turvin_gear = 0.5f;
     private float temperature;
     private float filtered;
 
     private float vacuum_speed_constant = 1.8f;
+
+    private AudioSource vacuum_sound;
+    private float vacuum_volume;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,24 +29,50 @@ public class vacuum_scene_control : MonoBehaviour
         dust.emissionRate = 0.0f;
         dirt.emissionRate = 0.0f;
         fine_dust.emissionRate = 0.0f;
+
+        filter_dust.emissionRate = 0.0f;
+
+        vacuum_sound = GetComponent<AudioSource>();
     }
     
     void Update()
     {
+        speed.value = (int)(speed.value);
         turvin_gear = speed.value;
 
+        // determining turvin speed with slider
         turbin.GetComponent<Animator>().speed = turvin_gear;
 
+        // up particles speed with turvin speed
         clip.startSpeed = vacuum_speed_constant * turvin_gear;
         dust.startSpeed = vacuum_speed_constant * turvin_gear;
         fine_dust.startSpeed = vacuum_speed_constant * turvin_gear;
         dirt.startSpeed = vacuum_speed_constant * turvin_gear;
 
+        // updating filter color
+        Material filter_color = hepa_filter.GetComponent<Renderer>().material;
+        if (filtered > 255) filtered = 255;
+        filter_color.color = new Color((255 - filtered)/255f, (255 - filtered) / 255f, (255 - filtered) / 255f);
+
+        //Debug.Log(filtered);
+
+        // when the filter is done (max dirt)
+        if (filtered > 200)
+        {
+            hepa_filter.transform.localPosition = new Vector3(Mathf.Sin(Time.time * 1000f) * 2 * (filtered) / 255f, Mathf.Sin(Time.time * 1000f) * 2 * (filtered) / 255f, 0);
+            filter_dust.emissionRate = 8;
+        }
+
+        //control sound//
+        if (!vacuum_sound.isPlaying && turvin_gear >= 1) vacuum_sound.Play();
+        else if (vacuum_sound.isPlaying && turvin_gear == 0) vacuum_sound.Stop();
+
+        if (vacuum_sound.isPlaying) vacuum_sound.pitch = 0.4f + turvin_gear * 0.3f;
     }
 
     public void particle_burst(int type)
     {
-        if(turvin_gear > 1f)
+        if(turvin_gear >= 1f)
         {
             switch (type)
             {
@@ -63,8 +95,7 @@ public class vacuum_scene_control : MonoBehaviour
 
     IEnumerator burst(ParticleSystem pc)
     {
-        Debug.Log("burst");
-        pc.emissionRate = 15f;
+        pc.emissionRate = 25f;
 
         while (pc.emissionRate > 0)
         {
@@ -76,5 +107,8 @@ public class vacuum_scene_control : MonoBehaviour
         if (pc.emissionRate < 0) pc.emissionRate = 0;
     }
 
-   
+    public void particle_filtered(int numb_particles)
+    {
+        filtered += numb_particles+1;
+    }
 }
