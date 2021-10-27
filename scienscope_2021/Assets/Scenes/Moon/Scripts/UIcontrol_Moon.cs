a76d72dd0df1e8b354e2d349e5f1b6825e704da9
Yes-Yeeun-Shin <81799147+Yes-Yeeun-Shin@users.noreply.github.com>
2021-10-27 14:17:39 +0900

    2021-10-27 ver3 upgrade
    - sound & description txt added

---
+++ scienscope_2021/Assets/Scenes/Moon/Scripts/UIcontrol_Moon.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIcontrol_Moon : MonoBehaviour
+{
+    public GameObject Cam;
+    public GameObject seatxt1;
+    public GameObject seatxt2;
+    public GameObject apollotxt1;
+    public GameObject apollotxt2;
+    public GameObject steptxt1;
+    public GameObject steptxt2;
+    private float distance;
+
+    void Update()
+    {
+        VisibleTxt(seatxt1, 350);
+        VisibleTxt(seatxt2, 350);
+        VisibleTxt(apollotxt1 , 200);
+        VisibleTxt(apollotxt2 , 200);
+        VisibleTxt(steptxt1 , 200);
+        VisibleTxt(steptxt2 , 200);
+    }
+
+    public void VisibleTxt(GameObject txt, float limit)
+    {   
+        distance = Vector3.Distance(txt.transform.position, Cam.transform.position);
+
+
+        if (distance <= limit)
+        {
+            txt.SetActive(true);
+        }
+    }
+}
