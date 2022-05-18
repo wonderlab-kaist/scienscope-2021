d10361faf06c080d26194424e48b76b720650e1a
Yes-Yeeun-Shin <81799147+Yes-Yeeun-Shin@users.noreply.github.com>
2022-05-18 21:08:08 +0900

    05-18 Body+Brain for demo

--- scienscope_2021/Assets/Scripts/TAG_loading.cs
+++ scienscope_2021/Assets/Scripts/TAG_loading.cs
@@ -37,30 +37,42 @@ public class TAG_loading : MonoBehaviour
     {
         stethoscope_data tmp = new stethoscope_data(dataInput.getData());
 
-        if (tmp!=null&&Application.platform == RuntimePlatform.Android)
+        if (tmp != null && Application.platform == RuntimePlatform.Android)
         {
             //heading.text = "rea";
             //Debug.Log(tmp);
-            scienscope_illust.transform.position = sc_illust_origin - new Vector3(0, (255 - tmp.distance) * 0.0025f, 0);
+            scienscope_illust.transform.position = sc_illust_origin + new Vector3(0, (tmp.distance) * 0.00125f, 0);
 
-
-            if (!(tmp.tag_id[0] == 0&& tmp.tag_id[1] == 0&& tmp.tag_id[2] == 0&& tmp.tag_id[3] == 0) && !scene_detected)
+            if (!(tmp.tag_id[0] == 0 && tmp.tag_id[1] == 0 && tmp.tag_id[2] == 0 && tmp.tag_id[3] == 0) && !scene_detected)
             {
                 Instantiate(ps_effect, ps_origin).transform.localPosition = Vector3.zero;
 
                 explain.text = "잠시만 기다려주세요...";
-                Debug.Log(System.BitConverter.ToString(tmp.tag_id).Replace("-",""));
+                Debug.Log(System.BitConverter.ToString(tmp.tag_id).Replace("-", ""));
                 string id = System.BitConverter.ToString(tmp.tag_id).Replace("-", "");
                 address.SetLastRFID(id); //save RFID Address for load in next scene
 
-                /// move on contents scenes ///
-                scene_detected = true;
-                SceneManager.LoadSceneAsync(1, LoadSceneMode.Single); ///i값을 원하는 scene의 build index로 대체
-                
-                
+                //explain.text = id;
+
+                for (int i = 0; i < RFID_address.Length; i++)
+                {
+                    string rfid_tmp = RFID_address[i].Replace(":", "");
+                    if (rfid_tmp.Equals(id))
+                    {
+                        scene_detected = true;
+                        SceneManager.LoadSceneAsync(i + 1, LoadSceneMode.Single); ///i값을 원하는 scene의 build index로 대체
+                    }
+
+                }
+
+                /// move on contents     scenes ///
+                //scene_detected = true;
+                //SceneManager.LoadSceneAsync("2_brain", LoadSceneMode.Single); ///i값을 원하는 scene의 build index로 대체
+
             }
         }
-
-        
     }
+
+
 }
+
